62e025667e0108743176cf8945af368daf40d3b1
sethycake <151950673+sethycake@users.noreply.github.com>
2024-01-23 20:16:39 +0100

    Add files via upload

---
+++ lol.py
@@ -0,0 +1,99 @@
+import random 
+import string
+
+code1 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code2 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code3 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code1+ '.'+ code2+ '.'+ code3)
+
+code4 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code5 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code6 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code4+ '.'+ code5+ '.'+ code6)
+
+code7 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code8 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code9 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code7+ '.'+ code8+ '.'+ code9)
+
+code10 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code11 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code12 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code10+ '.'+ code11+ '.'+ code12)
+
+code13 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code14 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code15 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code13+ '.'+ code14+ '.'+ code15)
+code16 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code17 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code18 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code16+ '.'+ code17+ '.'+ code18)
+code19 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code20 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code21 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code19+ '.'+ code20+ '.'+ code21)
+code22 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code23 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code24 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code22+ '.'+ code23+ '.'+ code24)
+code25 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code26 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code27 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code25+ '.'+ code26+ '.'+ code27)
+code28 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code29 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code30 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code28+ '.'+ code29+ '.'+ code30)
+code31 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code32 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code33 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code31+ '.'+ code32+ '.'+ code33)
+code34 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code35 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code36 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code34+ '.'+ code35+ '.'+ code36)
+code37 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code38 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code39 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code37+ '.'+ code38+ '.'+ code39)
+code40 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code41 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code42 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code40+ '.'+ code41+ '.'+ code42)
+code43 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code44 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code45 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code43+ '.'+ code44+ '.'+ code45)
+code46 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code47 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code48 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code46+ '.'+ code47+ '.'+ code48)
+code49 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code50 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code51 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code49+ '.'+ code50+ '.'+ code51)
+code52 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(22))
+code53 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
+code54 = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(38))
+
+print('MTE4M'+ code52+ '.'+ code53+ '.'+ code54)
+
+input('I hope ur token is valid!...')
